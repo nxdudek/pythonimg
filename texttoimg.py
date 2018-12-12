57311fdf817220827818ebf4dd3dcd9c09c7086f
unknown <nx.dudek@gmail.com>
2018-12-12 13:31:00 -0500

    cleaned up texttoimg a bit, and made it take string from 1st argument.

--- texttoimg.py
+++ texttoimg.py
@@ -1,29 +1,30 @@
 from PIL import Image
+import sys
 
-def fill(x, mx, y, (r, g, b), pixels):
+def fill(x, mx, y, r, g, b, pixels): # x is start x, mx is end x for the block
     for i in range(x, mx):
         for j in range(0, y):
             pixels[i, j] = (r, g, b)
 
-str = "this will be turned into a pattern of colours"
+str = sys.argv[1]
 blocksize = 50
 
-a = []
-cl = []
+temp = []
+colours = []
 
-for c in str:
-    q = ord(c)
-    a.append(q)
-    if (len(a) == 3):
-        cl.append(a)
-        a = []
+for c in str: # for each letter
+    temp.append(ord(c)) # add the unicode for the char to temporary array
+    if (len(temp) == 3): # every 3 characters
+        colours.append(temp) # store the colour
+        temp = []
 
-img = Image.new('RGB', (len(cl)*blocksize, blocksize))
+img = Image.new('RGB', (len(colours)*blocksize, blocksize)) # create an image based on number of colours
 pixels = img.load()
 
-for i in range(0, len(cl)):
+for i in range(0, len(colours)):
     for j in range(0, blocksize):
-        fill(i * blocksize, (i * blocksize) + blocksize, blocksize, (cl[i][0], cl[i][1], cl[i][2]), pixels)
+        fill(i * blocksize, (i * blocksize) + blocksize, blocksize, colours[i][0], colours[i][1], colours[i][2], pixels)
 
-print cl
+#print(colours)
+img.format = "PNG"
 img.show()
